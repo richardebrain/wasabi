310632f841bafa0647a337a66faf88ae788c2d50
richardebrain <richardebrain20@gmail.com>
2023-01-17 14:22:06 +0100

    feat :fixed data types error

--- src/helpers/constant.ts
+++ src/helpers/constant.ts
@@ -102,16 +102,84 @@ export const school: schoolTypes[] = [
 // ]
 
 type tableBodyProps = {
-	[key: string]: {
-		[key: string]: {
-			[key: string]: string | number | boolean
+	"FINDEET RATING": {
+		"STUDENT-TEACHER RATIO": {
+			school1: string;
+			school2: string;
+		};
+		"AVERAGE TEACHER SALARY": {
+			school1: string;
+			school2: string;
+		};
+		"ONLINE TEACHING": {
+			school1: string;
 
-		}
-	}
+			school2: string;
+		};
+	};
+	"KEY SCHOOL STATS": {
+		"SCHOOL ID CARD": {
+			school1: boolean;
+			school2: boolean;
+		};
+		"LIBRARY DURATION": {
+			school1: number;
+			school2: number;
+		};
+
+		"RESUMPTION DATE": {
+			school1: string;
+			school2: string;
+		};
+	};
+	"ACADEMIC STATS": {
+		"SCHOOL FEES": {
+			school1: string;
+			school2: string;
+		};
+		"LIBRARY STAT": {
+			school1: string;
+			school2: string;
+		};
+		"CLOSING DATE": {
+			school1: string;
+			school2: string;
+		};
+	};	
+	"TEACHER STATS": {
+		"SCHOOL UNIFORM": {
+			school1: string;
+			school2: string;
+		};
+		"STAT": {
+			school1: string;
+			school2: string;
+		};
+		"RESUMPTION DATE": {
+			school1: string;
+			school2: string;
+		};
+	};
+	"FEE STRUCTURE": {
+		"SCHOOL BUS": {
+			school1: string;
+			school2: string;
+		};
+		"LIBRARY BOOKS": {
+			school1: string;
+			school2: string;
+		};
+
+		"BREAK TIME": {
+			school1: string;
+			school2: string;
+		};
+	};
+
 
 
 }
-export const tableBodyArrayObject: tableBodyProps = {
+export const tableBodyArrayObject:tableBodyProps = {
 	"FINDEET RATING": {
 		"STUDENT-TEACHER RATIO": {
 			school1: "89",
@@ -182,27 +250,14 @@ export const tableBodyArrayObject: tableBodyProps = {
 			school2: "1pm",
 		},
 	},
-	"FEE ARRANGEMENT": {
-		"PART PAYMENT": {
-			school1: "200",
-			school2: "20",
-		},
-		"FULL PAYMENT": {
-			school1: "900",
-			school2: "900",
-		},
-		"BREAK TIME": {
-			school1: "2pm",
-			school2: "1pm",
-		},
-	},
+	
 }
 
 
 
 
 
-export const convertObjectToArray = (object: tableBodyProps | ArrayLike<tableBodyProps>) => {
+export const convertObjectToArray = (object: tableBodyProps) => {
 	// get the keys of the object
 	const keys = Object.keys(object);
 	// get the values of the object
